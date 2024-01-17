22eed7143c932e9f9bb4a27f7e9b94f6ee60d114
GRAF505 <124866580+GRAF505@users.noreply.github.com>
2024-01-17 16:08:10 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const printArrayElements = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+      console.log(arr[i]);
+    }
+  };
+  
+  const myArray = [1, 2, 3, 4, 5];
+  
+  printArrayElements(myArray);
